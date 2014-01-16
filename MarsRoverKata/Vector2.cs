7b4f01a57b20df4a4bb4b4ea566858db646dab7b
Brandon Griffin <bgriffin@qualtrax.com>
2014-01-16 13:13:43 -0500

    Added vectors for moving, and refactored some.

--- MarsRoverKata/Vector2.cs
+++ MarsRoverKata/Vector2.cs
@@ -54,5 +54,15 @@ namespace MarsRoverKata
         {
             return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
         }
+
+        public static Vector2 operator *(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X * v2.X, v1.Y * v2.Y);
+        }
+
+        public static Vector2 operator /(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X / v2.X, v1.Y / v2.Y);
+        }
     }
 }
